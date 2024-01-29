42b6084fa13fa3237e43a8f8a273b937eb99e196
unknown <adithk.bsc23@rvu.edu.in>
2024-01-29 20:28:25 +0530

    test

---
+++ set01/problem06.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void input(int *a, int *b, int *c) 
+{
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", a, b, c);
+}
+
+void compare(int a, int b, int c, int *largest) 
+{
+    if (a >= b && a >= c) {
+        *largest = a;
+    } else if (b >= a && b >= c) {
+        *largest = b;
+    } else {
+        *largest = c;
+    }
+}
+
+void output(int a, int b, int c, int largest) 
+{
+    printf("Numbers entered: %d, %d, %d\n", a, b, c);
+    printf("The largest number is: %d\n", largest);
+}
+
+int main() {
+    int num1, num2, num3, largest;
+
+    input(&num1, &num2, &num3);
+
+    compare(num1, num2, num3, &largest);
+
+    output(num1, num2, num3, largest);
+
+    return 0;
+}
