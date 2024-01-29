2619531540f2e3095d02747f6a42961591db7964
unknown <adithk.bsc23@rvu.edu.in>
2024-01-29 10:58:14 +0530

    test

--- set01/problem02.c
+++ set01/problem02.c
@@ -1,2 +1,11 @@
 //2. Write a C program to add two numbers.
-
+#include<stdio.h>
+int main()
+{
+    int x,y,z;
+    x=10;
+    y=100;
+    z=x+y;
+    printf("sun of x and y is %d",z);
+    return 0;
+}
