7cc60d022054337ff461ccf54f99d47c07516565
unknown <adithk.bsc23@rvu.edu.in>
2024-01-29 20:06:49 +0530

    test

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,44 @@
+#include<stdio.h>
 
+int input()
+{
+    int x;
+    printf("enter the number\n");
+    scanf("%d",&x);
+    return x;
+}
+
+int compare(int a,int b,int c)
+{
+    if (a >= b && a >= c) 
+    {
+        return a;
+    } else if (b >= a && b >= c) 
+    {
+        return b;
+    } else 
+    {
+        return c;
+    }
+}
+
+void output(int a,int b, int c, int largest)
+{
+    printf("Numbers entered: %d, %d, %d\n", a, b, c);
+    printf("The largest number is: %d\n", largest);
+}
+
+int main()
+{
+    int num1, num2, num3, largest;
+
+    num1 = input();
+    num2 = input();
+    num3 = input();
+
+    largest = compare(num1, num2, num3);
+
+    output(num1, num2, num3, largest);
+
+    return 0;
+}
