f3bb2e76023d582a20bc2c6650c5c8b6583be14a
unknown <adithk.bsc23@rvu.edu.in>
2024-02-01 10:13:37 +0530

    test

---
+++ set01/problem08.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int input_array_size() 
+{
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    return n;
+}
+
+void input_array(int n, int a[n])
+{
+    printf("Enter %d different numbers:\n", n);
+    for (int i = 0; i < n; ++i) {
+        printf("Enter number %d: ", i + 1);
+        scanf("%d", &a[i]);
+    }
+}
+
+int sum_n_array(int n, int a[n]) 
+{
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+        sum += a[i];
+    }
+    return sum;
+}
+
+void output(int n, int a[n], int sum) 
+{
+    printf("Numbers entered: ");
+    for (int i = 0; i < n; ++i) {
+        printf("%d ", a[i]);
+    }
+    printf("\nSum of the numbers: %d\n", sum);
+}
+
+int main()
+{
+    int n, sum;
+    n = input_array_size();
+    int numbers[n];
+    input_array(n, numbers);
+    sum = sum_n_array(n, numbers);
+    output(n, numbers, sum);
+    return 0;
+}
