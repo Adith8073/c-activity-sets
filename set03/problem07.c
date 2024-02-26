4679c3a85c3f6259efe05f15c69424e54b297c7e
unknown <adithk.bsc23@rvu.edu.in>
2024-02-26 15:46:00 +0530

    test

--- set03/problem07.c
+++ set03/problem07.c
@@ -2,26 +2,43 @@
 typedef struct point
 {
     float x, y;
-} Point;
+} point;
 
-typedef struct line {
-    Point p1, p2;
+typedef struct Line {
+    point p1, p2;
     float distance;
-} Line;
+} line;
 
-Point input_point()
+point input_point()
 {
+    point p;
+    printf("Enter the Points on X-axis AND Y-axis\n");
+    scanf("%f %f",&p.x,&p.y);
+    return p;
+}
+line input_line()
+{
+    line l;
+    l.p1=input_point();
+    l.p2=input_point();
+    return l;
+}
+void find_length(line *l)
+{
+    l->distance = sqrt(pow((l->p2.x - l->p1.x), 2) + pow((l->p2.y - l->p1.y), 2));          //d=√((x2 – x1)² + (y2 – y1)²)
+}
+void output(line l)
+{
+    printf("The Distance Between the %f,%f and %f,%f is : %f",l.p1.x,l.p1.y,l.p2.x,l.p2.y,l.distance);
+}
 
-}
-Line input_line()
+int main()
 {
-    
-}
-void find_length(Line *l)
-{
-    //d=√((x2 – x1)² + (y2 – y1)²)
-}
-void output(Line l)
-{
-
+    //no need to call the input_point() function as it is already called in input_line() function
+    // before initializing variable function should not be called.
+    line l;
+    l = input_line();
+    find_length(&l);
+    output(l);
+    return 0;
 }
