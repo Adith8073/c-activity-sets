c2f62ec76e48fa1e4acca9213b1f01e6df9902e0
unknown <adithk.bsc23@rvu.edu.in>
2024-02-27 15:45:50 +0530

    test

---
+++ set03/problem08.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+typedef struct Point {
+    float x,y;
+} point;
+
+
+typedef struct polygon {
+    int sides;
+    point p[100];
+    float perimeter;
+} Polygon;
+
+int input_n()
+{
+
+}
+point input_point(char *promt_msg)
+{
+
+}
+int input_polygon(Polygon *p)
+{
+
+}
+float find_distance(point a, point b)
+{
+
+}
+void find_perimeter(Polygon* p)
+{
+
+}
+void output(Polygon p)
+{
+     
+}
